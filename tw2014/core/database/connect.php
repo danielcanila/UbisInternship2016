528d06c08f3daaf6964b21e2ba1f80737d0637a9
Canila Ovidiu Daniel <daniel.canila@gmail.com>
2014-06-01 14:14:57 +0300

    Delete files and fix database connection
    delete files and update database connection

--- tw2014/core/database/connect.php
+++ tw2014/core/database/connect.php
@@ -1,5 +1,5 @@
 <?php
 $connect_error='You can\'t connect to the database';
 mysql_connect('localhost','root','') or die($connect_error);
-mysql_select_db('nelodb') or die ($connect_error);
+mysql_select_db('nelo') or die ($connect_error);
 ?>
