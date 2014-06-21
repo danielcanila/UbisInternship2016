b95a4c637d1239737b121919ebcb5bbe9c1f4d1b
Ionut <ionutdny9@gmail.com>
2014-06-21 23:01:54 +0300

    paypal
    am implementat partea de payment

--- tw2014/clientRegistration.php
+++ tw2014/clientRegistration.php
@@ -4,6 +4,9 @@ include 'core/init.php';
 
    
 
+$paypal_url='https://www.paypal.com/cgi-bin/webscr';
+$paypal_id='ionutdny9-facilitator@gmail.com'; // Business email ID
+					
 ?>
 
 <!DOCTYPE html>
@@ -58,7 +61,7 @@ include 'core/init.php';
      		 	 insertReservation($register_data);
      			 }
 			?>
-			   <form id="contact-form" action="contact.php" method="post">
+			   <form id="contact-form" action="clientRegistration.php" method="post">
 						<h2>Va rugam sa completati campurile de mai jos pentru a rezerva un loc in pensiunea noastra.</h2>
 						<div>
 							<label>
@@ -85,21 +88,32 @@ include 'core/init.php';
 							</label>
 						</div>	
 						<div>
-							<label>
-								<span>Camera: </span>
-									<select name="camera">
-									    <option value="1 persoana">1 persoane</option>
-									    <option value="2 persoane">2 persoane</option>
-									    <option value="3 persoane">3 persoane</option>
-									</select>
-							</label>
-						</div>	
-						<div>
 							<button name="rezerva" type="submit" id="rezerva">Rezerva Camera</button>
 						</div>
 					</form>
+					<div class="paypall">            
+						<form action="<?php echo $paypal_url; ?>" method="post" name="frmPayPal1">
+						<input type="hidden" name="business" value="<?php echo $paypal_id; ?>">
+					    <input type="hidden" name="cmd" value="_xclick">
+					    <input type="hidden" name="userid" value="1">
+					    <input type="hidden" name="amount" value="40">
+					    <input type="hidden" name="currency_code" value="EUR">
+					    <input type="hidden" name="handling" value="0">
+					    <input type="hidden" name="cancel_return" value="cancel.php">
+					    <input type="hidden" name="return" value="success.php">
+					    <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					    <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+						</form>
+					</div>
+					
+					   
+					    <div class="btn">
+					    
+					    </form> 
+					    </div>
+					</div>
 					</content>
-				
+
 				</article>
 		</div>
 	</div>
