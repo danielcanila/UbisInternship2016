9e9eceedbb999c21f1958c9c64293faaedb91c8c
Canila Ovidiu Daniel <daniel.canila@gmail.com>
2017-09-17 17:11:40 +0300

    [DC] here we merge changes from branch to master

--- HelloWorldGit/src/HelloWorld.java
+++ HelloWorldGit/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello all! Now we have both changes merged!");
 		System.out.println("Second change from site! This should not get conflict");
+		System.out.println("Let's do a conflic check from master side!");
 	}
 
 }
