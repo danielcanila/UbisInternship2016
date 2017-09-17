d420d9cbbb504ad1a773c1d2c238cba36209b612
Canila Ovidiu Daniel <daniel.canila@gmail.com>
2017-09-17 15:25:40 +0300

    [DC] check in after merge!

--- HelloWorldGit/src/HelloWorld.java
+++ HelloWorldGit/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello all! I changed this!");
+		System.out.println("Hello all! Now we have both changes merged!");
 	}
 
 }
