6ab909c03ed4611386b09cb20ef842d63d796495
Canila Ovidiu Daniel <daniel.canila@gmail.com>
2017-09-17 22:42:10 +0300

    [DC] check in for git from intellij

--- HelloWorldGit/src/HelloWorld.java
+++ HelloWorldGit/src/HelloWorld.java
@@ -1,14 +1,16 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello all! Now we have both changes merged!");
-		System.out.println("Second change from site! This should not get conflict");
-		System.out.println("Now lets stage a conflict! Revision from master & branch side!");
-		System.out.println("Changes i am going to loose.");
-		
-		// refresh me already!
-		// yet another set of changes from site!
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello all! Now we have both changes merged!");
+        System.out.println("Second change from site! This should not get conflict");
+        System.out.println("Now lets stage a conflict! Revision from master & branch side!");
+        System.out.println("Changes i am going to loose.");
+
+        // refresh me already!
+        // yet another set of changes from site!
+
+        // new change
+    }
 
 }
