8c19203b5fa25d8c70be178c6e62f7e872734512
Canila Ovidiu Daniel <daniel.canila@gmail.com>
2017-09-17 17:01:42 +0300

    [DC] change from site 2

--- HelloWorldGit/src/HelloWorld.java
+++ HelloWorldGit/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello all! Now we have both changes merged!");
+		System.out.println("Second change from site! This should not get conflict");
 	}
 
 }
