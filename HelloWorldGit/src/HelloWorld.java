a2dbc2407a868c55237fb27cf1b7129289cfbc8f
Canila Ovidiu Daniel <daniel.canila@gmail.com>
2017-09-17 17:51:25 +0300

    [DC] replacing branch with master version

--- HelloWorldGit/src/HelloWorld.java
+++ HelloWorldGit/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello all! Now we have both changes merged!");
 		System.out.println("Second change from site! This should not get conflict");
 		System.out.println("Now lets stage a conflict! Revision from master & branch side!");
+		System.out.println("Changes i am going to loose.");
 	}
 
 }
